89d70b8b7e4d25d107a34f7147ee5141916e3405
Tomas <bekjon_98@mail.ru>
2024-04-10 18:16:39 +0300

    update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,28 @@ export default {
 				light_blue: '#B9F0FF',
 				white: '#FBFBFB',
 				black: '#101010'
+			},
+			keyframes: {
+				'width-grow': {
+					'0%': { width: '25px' },
+					'25%': { width: '50px' },
+					'50%': { width: '75px' },
+					'75%': { width: '100px' },
+					'85%': { width: '150px' },
+					'100%': { width: 'fit-content' }
+				},
+				'width-shrink': {
+					'0%': { width: 'fit-content' },
+					'25%': { width: '150px' },
+					'50%': { width: '100px' },
+					'75%': { width: '75px' },
+					'85%': { width: '50px' },
+					'100%': { width: '25px' }
+				}
+			},
+			animation: {
+				'width-grow': 'width-grow .3s linear',
+				'width-shrink': 'width-shrink .3s linear',
 			}
 		},
 		fontFamily: {
