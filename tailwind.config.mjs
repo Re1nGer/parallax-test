9aa3e7cc15475d0995fe9c98d210b6cfc8f1abae
Tomas <bekjon_98@mail.ru>
2024-04-10 16:35:51 +0300

    update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				dark_purple: '#A092E1',
+				light_purple: '#CFC5FF',
+				dark_blue: '#97E9FF',
+				light_blue: '#B9F0FF',
+				white: '#FBFBFB',
+				black: '#101010'
+			}
+		},
 		fontFamily: {
 			valorax: ['valorax', 'sans-serif'],
 		},
