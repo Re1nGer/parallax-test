b25e0b917dd428c4b5f4867c0491a00abec214e4
Tomas <bekjon_98@mail.ru>
2024-04-12 22:07:49 +0300

    update modal windows for projects added

--- src/components/react/Project2Modal.jsx
+++ src/components/react/Project2Modal.jsx
@@ -1,6 +1,12 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {AnimatePresence, motion} from "framer-motion";
 import ProjectBorder from "../../assets/project_border.svg";
+import NetIcon from '../icons/.NET.jsx';
+import JestIcon from '../icons/Jest.jsx';
+import XUnitIcon from '../icons/Xunit.jsx';
+import ReactIcon from '../icons/React.jsx';
+import Network from '../../assets/Network.svg';
+
 import Project2 from "./Project2.jsx";
 
 const Project2Modal = () => {
@@ -10,24 +16,47 @@ const Project2Modal = () => {
     const handleClose = () => setIsOpen(false);
     const handleOpen = () => setIsOpen(true);
 
-
-    useEffect(() => {
-        document.body.style.overflow = isOpen ? "hidden" : "unset";
-    }, [isOpen]);
-
-
     return <>
         <Project2 onClick={handleOpen} />
         <AnimatePresence>
             { isOpen ? (
                 <>
                     <div className={'absolute h-[1000px] w-screen z-10 bg-black bg-opacity-0'} onClick={handleClose}></div>
-                    <div className={'absolute h-[600px] w-screen z-10 bg-black bg-opacity-100'}></div>
-                    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}  className={'relative flex justify-center w-full h-full items-center mb-[-4rem] z-10'}>
+                    <div className={'absolute max-h-[600px] h-full w-screen z-10 bg-black bg-opacity-100'}></div>
+                    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}  className={'relative flex justify-center flex-col w-full max-h-[2000px] mb-[8rem] items-center z-10'}>
                         <img src={ProjectBorder.src} className={'absolute z-20 max-w-[289px] max-h-[527px]'} onClick={(e) => e.stopPropagation()} alt={'project border'} />
-                        <div className={'max-w-[289px] flex flex-col items-center h-[480px] absolute'}>
-                            <p className={'text-white px-[25px] pt-6 mb-[22px] text-center relative z-20'}>Hotel accommodation system used by hotel managers to create and run campaigns, update availability of rooms etc</p>
-                            <h1 className={'text-dark_blue text-[25px] font-valorax blue-text-shadow'}>stack</h1>
+                        <div className={'max-h-[427px] overflow-y-auto w-full flex-col items-center flex absolute z-20'}>
+                            <div className={'max-w-[289px] flex flex-col relative z-10 items-center'}>
+                                <p className={'text-white px-[25px] mb-[10px] text-center relative z-20'}>Designed and built medical portal for conducting research with participants and medical professionals</p>
+                                <h1 className={'text-dark_blue text-[25px] font-valorax blue-text-shadow'}>stack:</h1>
+                            </div>
+                            <div className={'flex w-full justify-start flex-col gap-[12.5px] relative z-10'}>
+                                <div className={'flex justify-center gap-[12.5px]'}>
+                                    <NetIcon />
+                                    <ReactIcon />
+                                </div>
+                                <div className={'flex justify-center gap-[12.5px]'}>
+                                    <XUnitIcon />
+                                    <JestIcon />
+                                </div>
+                            </div>
+                            <div className={'flex gap-[.5rem] max-w-[250px] mt-[30px]'}>
+                                <img src={Network.src} className={'relative z-30 self-start'} alt={'n'} />
+                                <p className={'text-white text-left leading-none'}>Designed and developed SPA website from scratch using .NET 6, React, PostsgreSQL stack for medical research
+                                    team in Latvia</p>
+                            </div>
+                            <div className={'flex gap-[.5rem] max-w-[250px] mt-[30px]'}>
+                                <img src={Network.src} className={'relative z-30 self-start'} alt={'n'} />
+                                <p className={'text-white text-left leading-none'}>Introduced Selenium end-to-end tests in python as a part of CI/CD pipeline which resulted in less failed builds</p>
+                            </div>
+                            <div className={'flex gap-[.5rem] max-w-[250px] mt-[30px]'}>
+                                <img src={Network.src} className={'relative z-30 self-start'} alt={'n'} />
+                                <p className={'text-white text-left leading-none'}>Devised an antivirus microservice with ClamAV for file upload validation for custom CMS system</p>
+                            </div>
+                            <div className={'flex gap-[.5rem] max-w-[250px] mt-[30px]'}>
+                                <img src={Network.src} className={'relative z-30 self-start'} alt={'n'} />
+                                <p className={'text-white text-left leading-none'}>Implemented service worker for fetching audit messages from RabbitMQ</p>
+                            </div>
                         </div>
                     </motion.div>
                 </>
